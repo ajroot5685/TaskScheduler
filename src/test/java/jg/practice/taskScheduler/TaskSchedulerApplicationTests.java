d55f3cc64525717ccf3b45d2a9f52750a65172df
ajroot5685 <anjeong8577@naver.com>
2024-11-29 22:58:32 +0900

    init

---
+++ src/test/java/jg/practice/taskScheduler/TaskSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package jg.practice.taskScheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
